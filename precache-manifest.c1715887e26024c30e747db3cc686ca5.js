9ecf6fa74ee1800a4e8418cf5f274ebab0f65a29
Gabriel Seals <sealsgj@gmail.com>
2020-04-12 16:26:51 -0500

    Updates

---
+++ precache-manifest.c1715887e26024c30e747db3cc686ca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8ce55bc44f68b893b95f6b052d6541",
+    "url": "/React-in-github-demo/index.html"
+  },
+  {
+    "revision": "c0c524ea1d4d0fc22d61",
+    "url": "/React-in-github-demo/static/css/main.a1d1f803.chunk.css"
+  },
+  {
+    "revision": "427339010f8b577920a1",
+    "url": "/React-in-github-demo/static/js/2.693e80ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-github-demo/static/js/2.693e80ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c524ea1d4d0fc22d61",
+    "url": "/React-in-github-demo/static/js/main.d53b031a.chunk.js"
+  },
+  {
+    "revision": "d3a8cff586c596fa47e0",
+    "url": "/React-in-github-demo/static/js/runtime-main.26dc99d0.js"
+  }
+]);
